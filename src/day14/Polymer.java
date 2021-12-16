6f6c303a60fc8ff764217999db2b65f4f52a7726
Pontus Erlandsson <wnedpro@live.se>
2021-12-16 09:09:13 +0100

    solve day 14 A and B

--- src/day14/Polymer.java
+++ src/day14/Polymer.java
@@ -14,8 +14,9 @@ import adventofcode2021.AdventDay;
 
 public class Polymer implements AdventDay {
   final static private String path = "inputs/14.txt";
-  private HashMap<String, Integer> input = new HashMap<String, Integer>();
+  private HashMap<String, Long> input = new HashMap<String, Long>();
   private HashMap<String, String[]> formula = new HashMap<String, String[]>();
+  private String lastChar;
   
   public Polymer() {
     this.readInput();
@@ -31,13 +32,14 @@ public class Polymer implements AdventDay {
       String[] template = firstLine.split("");
       for(int i = 1; i < template.length; i++) {
         String pair = template[i-1] + template[i];
-        Integer existing = input.get(pair);
+        Long existing = input.get(pair);
         if(existing != null) {
           input.put(pair, ++existing);
         } else {
-          input.put(pair, 1);
+          input.put(pair, 1L);
         }
       }
+      lastChar = template[template.length - 1];
 
       String line;
       while ((line = inputStream.readLine()) != null) {
@@ -63,21 +65,18 @@ public class Polymer implements AdventDay {
 
   @SuppressWarnings("unchecked")
   public void runA() {
-    HashMap<String, Integer> polymer = (HashMap<String, Integer>) input.clone();
-    
-    // for each input value, go through and move the count onto the
-    // 2 new pairs created and put in new structure.
+    HashMap<String, Long> polymer = (HashMap<String, Long>) input.clone();
     
     for(int i = 0; i < 10; i++) {
-      HashMap<String, Integer> newPolymer = new HashMap<String, Integer>();
+      HashMap<String, Long> newPolymer = new HashMap<String, Long>();
       
       
-      Set<Entry<String, Integer>> set = polymer.entrySet();
-      for(Entry<String, Integer> ent: set) {
+      Set<Entry<String, Long>> set = polymer.entrySet();
+      for(Entry<String, Long> ent: set) {
         String[] newPoli = formula.get(ent.getKey());
         
         for(String poly: newPoli) {
-          Integer existing = newPolymer.get(poly);
+          Long existing = newPolymer.get(poly);
           if(existing != null) {
             newPolymer.put(poly, existing + ent.getValue());
           } else {
@@ -89,36 +88,66 @@ public class Polymer implements AdventDay {
       polymer = newPolymer;
     }
     
-    HashMap<String, Integer> usage = this.getElementUsage(polymer);
+    HashMap<String, Long> usage = this.getElementUsage(polymer);
     
-    Integer min = Collections.min(usage.values());
-    Integer max = Collections.max(usage.values());
+    Long min = Collections.min(usage.values());
+    Long max = Collections.max(usage.values());
     
     System.out.println("Day14 A: " + (max-min));
   }
 
+  @SuppressWarnings("unchecked")
   public void runB() {
-
+    HashMap<String, Long> polymer = (HashMap<String, Long>) input.clone();
+    
+    for(int i = 0; i < 40; i++) {
+      HashMap<String, Long> newPolymer = new HashMap<String, Long>();
+      
+      
+      Set<Entry<String, Long>> set = polymer.entrySet();
+      for(Entry<String, Long> ent: set) {
+        String[] newPoli = formula.get(ent.getKey());
+        
+        for(String poly: newPoli) {
+          Long existing = newPolymer.get(poly);
+          if(existing != null) {
+            newPolymer.put(poly, existing + ent.getValue());
+          } else {
+            newPolymer.put(poly, ent.getValue());            
+          }
+        }
+        
+      }
+      polymer = newPolymer;
+    }
+    
+    HashMap<String, Long> usage = this.getElementUsage(polymer);
+    
+    Long min = Collections.min(usage.values());
+    Long max = Collections.max(usage.values());
+    
+    System.out.println("Day14 B: " + (max-min));
   }
   
-  private HashMap<String, Integer> getElementUsage(HashMap<String, Integer> polymer) {
-  	HashMap<String, Integer> elements = new HashMap<String, Integer>();
+  private HashMap<String, Long> getElementUsage(HashMap<String, Long> polymer) {
+  	HashMap<String, Long> elements = new HashMap<String, Long>();
   	
-  	for(Entry<String, Integer> entry: polymer.entrySet()) {
+  	for(Entry<String, Long> entry: polymer.entrySet()) {
   		String key = entry.getKey();
-  		Integer amount = entry.getValue();
+  		Long amount = entry.getValue();
   		
-  		String[] keyParts = key.split("");
-
-  		for(String v: keyParts) {
-        Integer existing = elements.get(v);
-        if(existing != null) {
-        	elements.put(v, existing + amount);
-        } else {
-        	elements.put(v, amount);
-        }  			  			
-  		}
+  		String part = key.substring(0,1);
+  		
+  		Long existing = elements.get(part);
+      if(existing != null) {
+        elements.put(part, existing + amount);
+      } else {
+        elements.put(part, amount);
+      }
   	}
+  	
+  	Long a = elements.get(lastChar);
+  	elements.put(lastChar, ++a);
   	
     return elements;
   }
